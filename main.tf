86d7ae043c42637ce46f1f513086e1c38221eb14
amaresh0689 <40541131+amaresh0689@users.noreply.github.com>
2021-06-25 10:28:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -157,13 +157,6 @@ resource "aws_instance" "web" {
   # we specified
   ami = var.aws_amis[var.aws_region]
 
-  # The name of our SSH keypair you've created and downloaded
-  # from the AWS console.
-  #
-  # https://console.aws.amazon.com/ec2/v2/home?region=us-west-2#KeyPairs:
-  #
-  key_name = var.key_name
-
   # Our Security group to allow HTTP and SSH access
   vpc_security_group_ids = [aws_security_group.default.id]
   subnet_id              = aws_subnet.tf_test_subnet.id
